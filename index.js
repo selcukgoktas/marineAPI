81b1bba9b76ad69160679cb4e7681f8cb8cbe229
selcukgoktas <goktasselcuk1982@gmail.com>
2022-05-03 20:00:53 +0300

    donenv require

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
+require('dotenv').config();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +10,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(PORT, HOST, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
