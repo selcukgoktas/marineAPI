d7cd0f86cb7b88c4e6083482d2d0e43c436da804
selcukgoktas <goktasselcuk1982@gmail.com>
2022-05-03 20:37:43 +0300

    index.js converted to express server

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
-const http = require('http');
-require('dotenv').config();
+// Simple Express server setup to serve the build output
+//import path from "path";
+import express from "express";
+//import bodyParser from "body-parser";
+//import cors from "cors";
+import dotenv from "dotenv";
+//import allRouter from "./router/all.js";
+
+
+
+// HOST CONST
+dotenv.config();
 
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1453;
+const PROTOCOL = process.env.HTTPS === 'true' ? "https" : "http";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+
+const URL = `${PROTOCOL}://${HOST}:${PORT}`;
+
+
+
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static("public"));
+// app.use(cors());
+app.use(express.json());
+//app.use(connection);
+
+app.use(bodyParser.json({ limit: "100mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+
+
+// app.use(`/api/v1/all`, allRouter);
+
+
+app.get(/^(?!\/api).+/, (stdreq, stdres) => {
+	var query = stdreq.query;
+	console.log("Other Queries : ",query);
+	stdres.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
-});
+
+console.log(`✅ \x1b[32m Backend Server`);
+console.log(`💡 \x1b[33m Reactjs Build in Public`);
+console.log(` ❌ \x1b[31m System with Debug`);
+console.log(` 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥`);
+
+app.listen(PORT, () =>
+
+	console.log(`✅  \x1b[32m  Server started: ${URL}`)
+
+);
