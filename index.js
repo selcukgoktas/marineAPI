7c6611d01c903e73f78b606ec4d2e7e5cbb79e68
selcukgoktas <goktasselcuk1982@gmail.com>
2022-05-03 20:08:26 +0300

    Host variable changed

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
